702fb1a596042a84a8dcc99b303882e29f1fafd7
sidpd51 <sidpd510@gmail.com>
2024-09-07 19:00:33 +0530

    Completed city repository

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -6,11 +6,12 @@ class CityRepository {
             const city = await City.create({ name });
             return city;
         } catch (error) {
+            console.log("something went wrong in repository layer!");
             throw { error };
         }
     }
 
-    async deleteCity({ cityId }) {
+    async deleteCity(cityId) {
         try {
             await City.distroy({
                 where: {
@@ -18,6 +19,31 @@ class CityRepository {
                 },
             });
         } catch (error) {
+            console.log("something went wrong in repository layer!");
+            throw { error };
+        }
+    }
+
+    async updateCity(cityId, data) {
+        try {
+            const city = await City.update(data, {
+                where: {
+                    id: cityId,
+                },
+            });
+            return city;
+        } catch (error) {
+            console.log("something went wrong in repository layer!");
+            throw { error };
+        }
+    }
+
+    async getCity(cityId) {
+        try {
+            const city = await City.findByPk(cityId);
+            return city;
+        } catch (error) {
+            console.log("something went wrong in repository layer!");
             throw { error };
         }
     }
